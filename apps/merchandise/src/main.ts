0a4f972231810238fdd0add5b3c55af86dc589ff
ram1117 <ramkumark1@outlook.com>
2024-06-07 18:40:52 +0530

    add exceptions filter to capture mongodb duplicate key

--- apps/merchandise/src/main.ts
+++ apps/merchandise/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { MerchandiseModule } from './merchandise.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MongoExceptionsFilter } from '@app/shared/infrastructure/filters/mongoexceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(MerchandiseModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new MongoExceptionsFilter());
   await app.listen(3001);
 }
 bootstrap();
