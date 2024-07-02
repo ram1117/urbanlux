a728a20523a74f4c3c74b06eae15102b8e84cfe7
ram1117 <ramkumark1@outlook.com>
2024-07-02 08:31:22 +0530

    add brand and category endpoint in merchandise microservice

--- apps/merchandise/src/merchandise.controller.ts
+++ apps/merchandise/src/merchandise.controller.ts
@@ -35,9 +35,19 @@ export class MerchandiseController {
     return this.merchandiseService.findManyTopBrands();
   }
 
+  @Get('brands/:id')
+  getBrand(@Param('id') id: string) {
+    return this.merchandiseService.findOneBrand(id);
+  }
+
   @Get('category')
   getCategories() {
     return this.merchandiseService.findManyCategory();
+  }
+
+  @Get('category/:id')
+  getCategory(@Param('id') id: string) {
+    return this.merchandiseService.findOneCategory(id);
   }
 
   @Get('brandscategories')
